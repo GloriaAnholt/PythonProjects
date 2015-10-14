1fa7c4467b2e91fd02936917be1281c91f4c7b44
Gloria <totallygloria@users.noreply.github.com>
2015-10-14 15:55:27 -0700

    Update ex41_Study_Practice_for_Speaking_OOP.py

--- ex41_Study_Practice_for_Speaking_OOP.py
+++ ex41_Study_Practice_for_Speaking_OOP.py
@@ -17,30 +17,29 @@
 import random
 
 terms = {
-	'class': 'These are used (or "instantiated") to create objects. They have fields which represent quality the thing has, and methods which represent what the thing can do.',
-	'object': 'This has two possible meanings 1. The most basic type of thing, 2. Any instance of some thing.',
-	'instance': 'This is what you get when you tell Python to create a class.',
-	'def': 'The syntax used to make a function inside of a class',
-	'self': 'Inside the functions in a class, this is a variable for the instance or object being accessed.',
-	'inheritance': 'The concept that one class can receive the traits of another class, much like genetics.',
-	'composition': 'The concept that a class can have other classes as parts, much like how a car has wheels.',
-	'attribute': 'A property classes have that are from composition, are usually variables.',
-	'is-a': 'A phrase to say that something inherits from another class, as in a "salmon" **** "fish".',
-	'has-a': 'A phrase to say that something is composed of other things or contains trait, as in "a salmon **** mouth."'
-	}
+    'class': 'These are used (or "instantiated") to create objects. They have fields which represent quality the thing has, and methods which represent what the thing can do.',
+    'object': 'This has two possible meanings 1. The most basic type of thing, 2. Any instance of some thing.',
+    'instance': 'This is what you get when you tell Python to create a class.',
+    'def': 'The syntax used to make a function inside of a class',
+    'self': 'Inside the functions in a class, this is a variable for the instance or object being accessed.',
+    'inheritance': 'The concept that one class can receive the traits of another class, much like genetics.',
+    'composition': 'The concept that a class can have other classes as parts, much like how a car has wheels.',
+    'attribute': 'A property classes have that are from composition, are usually variables.',
+    'is-a': 'A phrase to say that something inherits from another class, as in a "salmon" **** "fish".',
+    'has-a': 'A phrase to say that something is composed of other things or contains trait, as in "a salmon **** mouth."'
+    }
 	
 	
 items_total = range(0, len(terms))
 
 while len(items_total) != 0:
-	random.shuffle(items_total)
-	i = items_total[-1]
-	print terms.values()[i] 
-	guess = raw_input('Term: ').lower()
-
-	if guess == terms.keys()[i]:
-		items_total.pop()
-		print "Correct! \n"
-	else:
-		print "Try again. \n"
-		
+    random.shuffle(items_total)
+    i = items_total[-1]
+    print terms.values()[i] 
+    guess = raw_input('Term: ').lower()
+    
+    if guess == terms.keys()[i]:
+        items_total.pop()
+        print "Correct! \n"
+    else:
+        print "Try again. \n"
