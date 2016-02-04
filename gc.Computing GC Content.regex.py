f9ddbfc50e2d207b921390fb4a50b65314a2dc09
Gloria Guy <totallygloria@gmail.com>
2016-02-04 14:46:01 -0800

    Changed to using named capture groups in the regex to improve readability. Tried several different ways of using the dict to solve the problem, none worked. Left all in the code commented out for future attempts.

--- gc.Computing GC Content.regex.py
+++ gc.Computing GC Content.regex.py
@@ -1,8 +1,9 @@
 # Rodalind: Computing GC Content
-# 11.30.15
+# 12.04.15
 # @totallygloria
 
 import re
+import collections
 
 in_file = open("gc.Computing GC Content.data.py", "r")
 raw_data = in_file.read()
@@ -10,27 +11,62 @@ raw_data = raw_data.replace('\n', '')
 
 def build_strand_dicts(raw_data):					
 
-	massaged_data = re.findall('>(Rosalind_[0-9]{4})([CGAT]+)', raw_data)
+	massaged_data = re.findall('>(?P<title>Rosalind_[0-9]{4})(?P<bases>[ACGT+]+)', raw_data)
 	strand_list = []
-	for k,v in massaged_data:
-		strand_len = len(v)
-		content = 0
-		for base in v:
-			if base in "CG":
-				content += 1
-		strand_list.append({
-							"title": k, "strand": v, 
-							"length": strand_len, "cg content": content})			
+	for x,y in massaged_data:
+		strand_list.append({"title": x, "bases": y})			
 	return strand_list
 	
 def most_cg_pairs(strands):
 	
-	pass
-
-
-
-strand_dict = build_strand_dicts(raw_data)
-
-for d in strand_dict:
-	print d
+	# This doesn't work because it sums ALL of the letters together
+	# but it otherwise an interesting idea
+	'''
+	test = collections.Counter()
+	for d in strands:
+		test.update(d["strand"])
+	for letter, count in test.most_common(4):
+		print '%s: %7d' % (letter, count)
+	'''
+	# Trying to work out the logic in a loop to see if I can escape
+	# using loops. Can't so far.
+	'''
+	content = []
+	for d in strands:
+		counter = 0
+		total = 0
+		for base in d["strand"]:
+			if base:
+				total += 1
+			if base in "CG":
+				counter += 1
+		content.append([total, counter])
+	print content
+	'''
+	# Still using for loops, not they're a lot less clear. Trying to use
+	# .count and max(), but haven't really figured it out. 
+	'''
+	test = []
+	for d in strands:
+		test.append([d["strand"].count(""), d["strand"].count("C") + d["strand"].count("G")])
+	for i in test:
+		print (float(i[1])/float(i[0]))
+	'''
+	# Trying making a letter-counting dictionary for each dictionary
+	# It works, but it looks like a total PITA to tabulate totals and
+	# do my division. Still not happy.
+	'''	
+	new_dict = {}
+	
+	for d in strands:
+		title = d["title"]
+		print title
+		new_dict[title]= collections.Counter(d["bases"])	
+	return new_dict	
+	'''
+	
+	
+	
+#strand_list = build_strand_dicts(raw_data)
+#most_cg_pairs(strand_list)
 
