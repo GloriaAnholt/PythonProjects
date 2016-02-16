3918f452cf1d65862a9315e51ae027428b81581f
Gloria Guy <totallygloria@gmail.com>
2016-02-15 16:00:40 -0800

    Outlined member functions of Deque class with descriptions.

---
+++ Algorithms-DataStructures/DequeClass.py
@@ -0,0 +1,34 @@
+# Algorithms & Data Structures: Deque
+# 02.15.16
+# @totallygloria
+
+
+
+class Deque(object):
+    # creates a new deque that is empty. It needs no parameters and returns an empty deque.
+
+    def addfront(item):
+        # adds a new item to the front of the deque. It needs the item and returns nothing.
+        pass
+
+    def addrear(item): 
+        # adds a new item to the rear of the deque. It needs the item and returns nothing.
+        pass
+
+    def removefront():
+        # removes the front item from the deque. It needs no parameters and returns the item.
+        # The deque is modified.
+        pass
+
+    def removerear():
+        # removes the rear item from the deque. It needs no parameters and returns the item.
+        # The deque is modified.
+        pass
+
+    def isempty():
+        # tests to see whether the deque is empty. It needs no parameters and returns a boolean value.
+        pass
+
+    def size():
+        # returns the number of items in the deque. It needs no parameters and returns an integer.
+
