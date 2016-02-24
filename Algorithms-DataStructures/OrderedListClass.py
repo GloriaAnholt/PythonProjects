7a0afc9ba8f202f07f28d89c7e1358dfb1c63e3e
Gloria Guy <totallygloria@gmail.com>
2016-02-24 13:51:04 -0800

    Modifying the ListClass to be an Ordered List (ie, sorted). Starting with the existing code and modifying from there. Creating a new empty ordered list passes.

---
+++ Algorithms-DataStructures/OrderedListClass.py
@@ -0,0 +1,151 @@
+# Data Structures and Algorithms: List Class
+# 02.24.16
+# @totallygloria
+
+
+class OrderedList(object):
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.size = 0
+
+    def add(self, item):
+        # adds a new item to the list making sure that the order is preserved. It needs the item
+        # and returns nothing. Assume the item is not already in the list.
+        new_node = Node(item)
+        if self.size == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            current = self.first
+            for i in range(self.size + 1):
+                if new_node.item < current.item:
+                    current = current.ahead
+                elif new_node.item >= current.item:
+                    ahead = current.ahead
+                    new_node.behind = current
+                    new_node.ahead = ahead
+                    ahead.behind = new_node
+                    current.ahead = new_node
+        self.size += 1
+
+    def remove(self, item):
+        # removes the item from the list. It needs the item and modifies the list.
+        # Assume the item is present in the list.
+        current = self.first
+        for i in range(self.size):
+            if current.item == item and i == 0:
+                inback = current.behind
+                inback.ahead = None
+                self.first = inback
+                self.size -= 1
+                break
+            elif current.item == item and i == (self.size - 1):
+                infront = current.ahead
+                infront.behind = None
+                self.last = infront
+                self.size -= 1
+                break
+            elif current.item == item:
+                infront = current.ahead
+                inback = current.behind
+                infront.behind = inback
+                inback.ahead = infront
+                self.size -= 1
+                break
+            else:
+                current = current.behind
+
+    def search(self, item):
+        # searches for the item in the list. It needs the item and returns a boolean value.
+        current = self.first
+        for i in range(self.size):
+            if current.item == item:
+                return True
+            else:
+                current = current.behind
+        return False
+
+    def is_empty(self):
+        # tests to see whether the list is empty. It needs no parameters and returns a boolean value.
+        if self.size == 0 or (self.first == None and self.last == None):
+            return True
+        return False
+
+    def size(self):
+        # returns the number of items in the list. It needs no parameters and returns an integer.
+        return self.size
+
+    def index(self, item):
+        # returns the position of item in the list. It needs the item and returns the index.
+        # Assume the item is in the list.
+        current = self.first
+        for i in range(self.size):
+            if current.item == item:
+                return i
+            else:
+                current = current.behind
+        return None
+
+    def pop(self):
+        # removes and returns the last item in the list. It needs nothing and returns an item.
+        # Assume the list has at least one item.
+        if self.size > 1:
+            popped = self.last
+            new_last = popped.ahead
+            new_last.behind = None
+            self.last = new_last
+            self.size -= 1
+            return popped.item
+        elif self.size == 1:
+            popped = self.last
+            self.last = None
+            self.first = None
+            self.size -= 1
+            return popped.item
+        return None
+
+    def grab(self, pos):
+        # removes and returns the item at position pos. It needs the position and returns the item.
+        # Assume the item is in the list.
+        # Special case for first item, doesn't need to loop
+        if pos == 0 and self.size == 1:   # Clear the list if only one node long
+            current = self.first
+            self.last = None
+            self.first = None
+            self.size -= 1
+            return current.item
+        elif pos == 0 and self.size > 1:    # Reset front if longer than one
+            current = self.first
+            new_first = current.behind
+            new_first.ahead = None
+            self.first = new_first
+            self.size -= 1
+            return current.item
+        # Special case for last item, calls pop method
+        elif pos == (self.size - 1):
+            return self.pop()
+        # Otherwise, loop through list until at item, then remove and return item
+        current = self.first
+        for i in range(pos + 1):
+            if i == pos:
+                infront = current.ahead
+                inrear = current.behind
+                infront.behind = inrear
+                inrear.front = infront
+                self.size -= 1
+                return current.item
+            else:
+                current = current.behind
+
+
+class Node(object):
+
+    def __init__(self, value):
+        self.ahead = None
+        self.behind = None
+        self.item = value
+
+    def get_next(self):
+        return self.behind
---
+++ Algorithms-DataStructures/test_OrderedListClass.py
@@ -0,0 +1,190 @@
+# Data Structures and Algorithms: ListClass
+# 02.18.16
+# @totallygloria
+
+
+import unittest
+# import sys
+# from StringIO import StringIO
+
+from OrderedListClass import OrderedList
+
+
+class OrderedListClassTester(unittest.TestCase):
+
+    def test_new_ordered_list(self):
+        l = OrderedList()
+        self.assertEqual(l.size, 0)
+        self.assertEqual(l.first, None)
+        self.assertEqual(l.last, None)
+
+    def test_add(self):
+        l = OrderedList()
+        l.add(7)
+        self.assertEqual(l.size, 1)
+        self.assertEqual(l.first.item, 7)
+        self.assertEqual(l.last.item, 7)
+        l.add(4)
+        self.assertEqual(l.size, 2)
+        self.assertEqual(l.first.item, 4)
+        self.assertEqual(l.last.item, 7)
+        l.add(5)
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.first.item, 4)
+        self.assertEqual(l.last.item, 7)
+'''
+    def test_pop(self):
+        l = OrderedList()
+        l.add('apple')
+        l.add('banana')
+        l.add('coconut')
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.pop(), 'apple')
+        self.assertEqual(l.size, 2)
+        self.assertEqual(l.pop(), 'banana')
+        self.assertEqual(l.size, 1)
+        self.assertEqual(l.pop(), 'coconut')
+        self.assertEqual(l.size, 0)
+        self.assertEqual(l.pop(), None)
+
+    def test_remove(self):
+        # Build a list, check that it is as expected
+        l = OrderedList()
+        l.add('apple')
+        l.append('banana')
+        l.append('coconut')
+        self.assertEqual(l.size, 3)
+
+        # Case: remove from front of list
+        self.assertEqual(l.first.item, 'apple')
+        l.remove('apple')
+        self.assertEqual(l.first.item, 'banana')
+        self.assertEqual(l.size, 2)
+
+        # Case: remove from end of list
+        l.append('durian')
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.last.item, 'durian')
+        l.remove('durian')
+        self.assertEqual(l.size, 2)
+        self.assertEqual(l.last.item, 'coconut')
+        self.assertEqual(l.first.item, 'banana')
+
+        # Case: remove from middle of list
+        l.append('elderberry')
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.last.item, 'elderberry')
+        l.remove('coconut')
+        self.assertEqual(l.size, 2)
+        self.assertEqual(l.first.item, 'banana')
+        self.assertEqual(l.last.item, 'elderberry')
+
+    def test_search(self):
+        l = OrderedList()
+        l.add('apple')
+        l.append('banana')
+        l.append('coconut')
+        l.append('durian')
+        l.append('elderberry')
+        self.assertEqual(l.size, 5)
+        self.assertTrue(l.search('apple'))
+        self.assertTrue(l.search('banana'))
+        self.assertTrue(l.search('coconut'))
+        self.assertTrue(l.search('durian'))
+        self.assertTrue(l.search('elderberry'))
+        self.assertFalse(l.search(None))
+        self.assertFalse(l.search('peach'))
+
+    def test_is_empty(self):
+        l = OrderedList()
+        self.assertTrue(l.is_empty())
+        l.add('apple')
+        self.assertFalse(l.is_empty())
+        l.pop()
+        self.assertTrue(l.is_empty())
+
+    def test_index(self):
+        l = OrderedList()
+        self.assertEqual(l.index('peach'), None)
+        l.add('apple')
+        l.append('banana')
+        l.append('coconut')
+        l.append('durian')
+        l.append('elderberry')
+        self.assertEqual(l.index('apple'), 0)
+        self.assertEqual(l.index('banana'), 1)
+        self.assertEqual(l.index('coconut'), 2)
+        self.assertEqual(l.index('durian'), 3)
+        self.assertEqual(l.index('elderberry'), 4)
+        self.assertEqual(l.index('peach'), None)
+
+    def test_grab(self):
+        # Build a list, check that it is as expected
+        l = OrderedList()
+        l.add('apple')
+        l.append('banana')
+        l.append('coconut')
+        self.assertEqual(l.size, 3)
+
+        # Case: grab from front of list
+        self.assertEqual(l.grab(0), 'apple')
+        self.assertEqual(l.size, 2)
+        self.assertEqual(l.first.item, 'banana')
+        self.assertEqual(l.last.item, 'coconut')
+
+        # Case: grab from end of list
+        l.append('durian')
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.grab(2), 'durian')
+        self.assertEqual(l.first.item, 'banana')
+        self.assertEqual(l.last.item, 'coconut')
+
+        # Case: grab from middle of list
+        l.append('elderberry')
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.grab(1), 'coconut')
+        self.assertEqual(l.first.item, 'banana')
+        self.assertEqual(l.last.item, 'elderberry')
+
+    def test_insert(self):
+        l = OrderedList()
+        # Case: insert into empty list
+        l.insert(0, 'apricot')
+        self.assertEqual(l.first.item, 'apricot')
+        self.assertEqual(l.last.item, 'apricot')
+        self.assertEqual(l.size, 1)
+
+        # Case: insert at a pos longer than the list
+        l.insert(100, 'wintergreen')
+        self.assertEqual(l.first.item, 'apricot')
+        self.assertEqual(l.last.item, 'wintergreen')
+        self.assertEqual(l.size, 2)
+
+        # Case: insert into front of list
+        l.insert(0, 'apple')
+        self.assertEqual(l.first.item, 'apple')
+        self.assertEqual(l.size, 3)
+
+        # Case: insert into middle of list
+        l.insert(2, 'banana')
+        self.assertEqual(l.size, 4)
+        self.assertEqual(l.first.item, 'apple')
+        self.assertEqual(l.last.item, 'wintergreen')
+        self.assertTrue(l.search('banana'))
+
+        # Checking that order is as expected
+        self.assertEqual(l.pop(), 'wintergreen')
+        self.assertEqual(l.pop(), 'banana')
+        self.assertEqual(l.pop(), 'apricot')
+        self.assertEqual(l.pop(), 'apple')
+        self.assertEqual(l.pop(), None)
+
+        # Case, inserting at a weird index into empty list
+        l.insert(42, 'apricot')
+        self.assertEqual(l.first.item, 'apricot')
+        self.assertEqual(l.last.item, 'apricot')
+        self.assertEqual(l.size, 1)
+'''
+
+if __name__ == '__main__':
+    unittest.main()
