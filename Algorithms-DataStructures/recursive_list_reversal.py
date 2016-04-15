b99e90ace6e4db9f550c7eb969595510850acc4e
Gloria Guy <totallygloria@gmail.com>
2016-04-15 12:27:32 -0700

    Fixed my recursive nesting of lists - need to pass the list itself when making the function call. Passes some basic tests w/ ints and strings.

--- Algorithms-DataStructures/recursive_list_reversal.py
+++ Algorithms-DataStructures/recursive_list_reversal.py
@@ -3,25 +3,42 @@
 # @totallygloria
 
 
+import unittest
+
 '''
 Write a recursive function to reverse a list.
 '''
 
 
-def rev_list(sample):
-	
-	new_list = []
-	
-	if len(sample) == 1:
-		new_list.append(sample[0])
-	else:
-		new_list.append(sample[-1])
-		sample.pop()
-		new_list.append(rev_list(sample))
-		
-	return new_list
-	
+def reverser(original_list, reversed_list):
 
-print rev_list([1,2,3,4,5])
+    if len(original_list) == 1:
+        reversed_list.append(original_list[0])
+    else:
+        lastnum = original_list.pop()
+        reversed_list.append(lastnum)
+        reverser(original_list, reversed_list)
 
-		
+    return reversed_list
+
+
+
+
+
+class ReverserTester(unittest.TestCase):
+
+    def reverser(self):
+        self.assertEqual(reverser([], []), [])
+        self.assertEqual(reverser([1], []), [1])
+        self.assertEqual(reverser([1,2,3,4,5], []), [5,4,3,2,1])
+        self.assertEqual(reverser(['a','b','c','d'], []), ['d','c','b','a'])
+        
+        self.assertNotEquals(reverser([1,2,3,4,5], []), [1,2,3,4,5])
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
