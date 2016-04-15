b948c946bb2cb1018851b4ae1740beda801a1a46
Gloria Guy <totallygloria@gmail.com>
2016-04-15 12:33:45 -0700

    Cleaned up spacing, added tests - passes neg, 0, 1, and pos value cases.

--- Algorithms-DataStructures/recursive_factorial.py
+++ Algorithms-DataStructures/recursive_factorial.py
@@ -3,21 +3,37 @@
 # @totallygloria
 
 
+import unittest
+
+
 '''
 Write a recursive function to compute the factorial of a number
 '''
 
 
 def factorial_calc(num):
-	
-	if num <= 1:
-		return 1
-	else:
-		total = num * factorial_calc(num-1)
-	
-	return total 
-	
+
+    if num <= 1:
+        return 1
+    else:
+        total = num * factorial_calc(num-1)
+
+    return total
 
 
+class Factorial_Calc_Tester(unittest.TestCase):
 
-		
+    def factorial_calc(self):
+        self.assertEqual(factorial_calc(0), 1)
+        self.assertEqual(factorial_calc(-243), 1)
+        self.assertEqual(factorial_calc(2), 2)
+        self.assertEqual(factorial_calc(5), 120)
+        self.assertEqual(factorial_calc(9), 362880)
+
+        self.assertNotEquals(factorial_calc(4), 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
