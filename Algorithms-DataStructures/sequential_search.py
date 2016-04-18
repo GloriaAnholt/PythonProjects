0b23b937624cf0c30b1896be4dc7e99564218546
Gloria Guy <totallygloria@gmail.com>
2016-04-18 11:36:05 -0700

    Used enumerate to track current index in list, now returns a tuple with boolean and pos.
    Tried to have a separate counter, but an empty list would or non-found would have an
    off-by-one error, depending on where I did the increment. enumerate was the obvious fix,
    but I'm unclear on how to do it manually.

--- Algorithms-DataStructures/sequential_search.py
+++ Algorithms-DataStructures/sequential_search.py
@@ -18,29 +18,28 @@ def seq_search(items, wanted):
     located = False
     pos = 0
 
-    for item in items:
+    for pos, item in enumerate(items):
         if item == wanted:
             located = True
             break
-        pos += 1
 
-    return located
+    return (located, pos)
 
 
 class Sec_Search_Tester(unittest.TestCase):
 
     def test_seq_search(self):
-        self.assertEquals(seq_search([], ''), False)
-        self.assertNotEquals(seq_search([], ''), True)
-        self.assertEquals(seq_search([], '1'), False)
-        self.assertEquals(seq_search(['1'], '1'), True)
-        self.assertEquals(seq_search(['1'], 1), False)
-        self.assertEquals(seq_search([1], 1), True)
-        self.assertEquals(seq_search([5,4,3,2,1], 1), True)
-        self.assertEquals(seq_search([5,4,3,2,1], 11), False)
-        self.assertEquals(seq_search([5,4, None, 'a', 3,2,'whelp',1], 'a'), True)
-        self.assertEquals(seq_search([5,4, None, 'a', 3,2,'whelp',1], None), True)
-        self.assertEquals(seq_search([5,4, None, [1,2,3], 'a', 3,2,'whelp',1], [1,2,3]), True)
+        self.assertEquals(seq_search([], ''), (False, 0))
+        self.assertNotEquals(seq_search([], ''), (True, 0))
+        self.assertEquals(seq_search([], '1'), (False, 0))
+        self.assertEquals(seq_search(['1'], '1'), (True, 0))
+        self.assertEquals(seq_search(['1'], 1), (False, 0))
+        self.assertEquals(seq_search([1], 1), (True, 0))
+        self.assertEquals(seq_search([5,4,3,2,1], 1), (True, 4))
+        self.assertEquals(seq_search([5,4,3,2,1], 11), (False, 4))
+        self.assertEquals(seq_search([5,4, None, 'a', 3,2,'whelp',1], 'a'), (True, 3))
+        self.assertEquals(seq_search([5,4, None, 'a', 3,2,'whelp',1], None), (True, 2))
+        self.assertEquals(seq_search([5,4, None, [1,2,3], 'a', 3,2,'whelp',1], [1,2,3]), (True, 3))
 
 
 
