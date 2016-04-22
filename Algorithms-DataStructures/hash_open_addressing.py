7f82fda3485ecebfdb079ccc648e1b3bf377a8f4
Gloria Guy <totallygloria@gmail.com>
2016-04-21 18:39:26 -0700

    Added a remove item function -- looks in the appropriate slot, if it's there, removes it and replaces with 'USED' and returns True; if it's None returns False; otherwise, walks the list until it gets to the first None. Passes tests.
    I don't know if it wraps around the list, need to test that.

--- Algorithms-DataStructures/hash_open_addressing.py
+++ Algorithms-DataStructures/hash_open_addressing.py
@@ -5,7 +5,7 @@
 """
 Uses a python list to hold the values in the hash table.
 Hash function is a basic module arithmetic:
-   h(item) = item ^ 2 % table_size
+   h(item) = item**2 % table_size
 If there's a collision, resolves using open addressing.
 """
 
@@ -19,8 +19,8 @@ def insert_hash(item, table):
     # Hashes item, if the slot is empty it inserts, otherwise it walks the table
     # until it finds and open slot and puts it there.
     size = len(table)
-    hashed_val = item^2 % size
-    if table[hashed_val] is None:
+    hashed_val = item**2 % size
+    if table[hashed_val] is None or table[hashed_val] == 'USED':
         table[hashed_val] = item
     else:
         while table[hashed_val] is not None:
@@ -30,7 +30,7 @@ def insert_hash(item, table):
 def search_hash(item, table):
     # Hashes item, checks if it's in the hash table - if it's not where it's expected
     # to be, walks the table until a None is found. Returns a boolean.
-    hashed_val = item^2 % len(table)
+    hashed_val = item**2 % len(table)
     if table[hashed_val] == item:
         return True
     elif table[hashed_val] == None:
@@ -41,7 +41,25 @@ def search_hash(item, table):
             while table[hashed_val] is not None:
                 if table[hashed_val] == item:
                     return True
+                hashed_val += 1
         return False
 
-
-
+def remove_item(item, table):
+    # Searches for an item in the hash table, if it's not in the proper slot,
+    # checks until you hit the first None. When the item is found, it's replaced
+    # with a marker and returns a boolean.
+    hashed_val = item**2 % len(table)
+    if table[hashed_val] == item:
+        table[hashed_val] = 'USED'
+        return True
+    elif table[hashed_val] == None:
+        return False
+    else:
+         if table[hashed_val] != item:
+            hashed_val += 1
+            while table[hashed_val] is not None:
+                if table[hashed_val] == item:
+                    table[hashed_val] = 'USED'
+                    return True
+                hashed_val += 1
+         return False
--- Algorithms-DataStructures/test_hash-open_addressing.py
+++ Algorithms-DataStructures/test_hash-open_addressing.py
@@ -5,7 +5,7 @@
 
 
 import unittest
-from hash_open_addressing import create_table, insert_hash, search_hash
+from hash_open_addressing import create_table, insert_hash, search_hash, remove_item
 
 
 class HashClassTester(unittest.TestCase):
@@ -16,8 +16,6 @@ class HashClassTester(unittest.TestCase):
         self.assertEqual(create_table(2), [None, None, None, None])
         self.assertEqual(create_table(8), ([None]*16))
 
-
-'''
     def test_insert_hash(self):
         h = create_table(5)
         insert_hash(10, h)
@@ -25,27 +23,41 @@ class HashClassTester(unittest.TestCase):
         insert_hash(11, h)
         self.assertEqual(h, [10, 11, None, None, None, None, None, None, None, None])
         insert_hash(12, h)
-        self.assertEqual(h, [10, 11, None, 12, None, None, None, None, None, None])
-        insert_hash(17, h)
-        self.assertEqual(h, [10, 11, 12, None, None, None, None, 17, None, None])
+        self.assertEqual(h, [10, 11, None, None, 12, None, None, None, None, None])
+        insert_hash(8, h)
+        self.assertEqual(h, [10, 11, None, None, 12, 8, None, None, None, None])
         insert_hash(109, h)
-        self.assertEqual(h, [10, 11, 12, None, None, None, None, 17, None, 109])
-
-
-
+        self.assertEqual(h, [10, 11, 109, None, 12, 8, None, None, None, None])
 
     def test_search_hash(self):
         h = create_table(5)
-        extend_hash([10,11,12,106,108,109], h)
-        extend_hash([10,20,30,40], h)
+        insert_hash(10, h), insert_hash(11, h), insert_hash(12, h)
+        insert_hash(8, h), insert_hash(109, h)
+        self.assertEqual(h, [10, 11, 109, None, 12, 8, None, None, None, None])
         self.assertTrue(search_hash(10, h))
-        self.assertTrue(search_hash(20, h))
-        self.assertTrue(search_hash(30, h))
-        self.assertTrue(search_hash(106, h))
+        self.assertTrue(search_hash(11, h))
         self.assertTrue(search_hash(109, h))
-        self.assertFalse(search_hash(50, h))
-        self.assertFalse(search_hash(107, h))
-'''
+        self.assertTrue(search_hash(12, h))
+        self.assertTrue(search_hash(8, h))
+
+    def test_remove_item(self):
+        h = create_table(5)
+        insert_hash(10, h), insert_hash(11, h), insert_hash(12, h)
+        insert_hash(8, h), insert_hash(109, h)
+        self.assertEqual(h, [10, 11, 109, None, 12, 8, None, None, None, None])
+        self.assertTrue(remove_item(10, h))
+        self.assertEqual(h, ['USED', 11, 109, None, 12, 8, None, None, None, None])
+        self.assertFalse(search_hash(10, h))
+        self.assertTrue(remove_item(109, h))
+        self.assertEqual(h, ['USED', 11, 'USED', None, 12, 8, None, None, None, None])
+        self.assertFalse(search_hash(109, h))
+        self.assertTrue(remove_item(8, h))
+        self.assertEqual(h, ['USED', 11, 'USED', None, 12, 'USED', None, None, None, None])
+        self.assertFalse(search_hash(8, h))
+        insert_hash(10, h)
+        self.assertEqual(h, [10, 11, 'USED', None, 12, 'USED', None, None, None, None])
+        self.assertTrue(search_hash(10, h))
+
 
 if __name__ == '__main__':
     unittest.main()
