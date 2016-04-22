a105ab72583cef2ff3648ed2e799794172f03bb3
Gloria Guy <totallygloria@gmail.com>
2016-04-21 17:54:52 -0700

    Wrote another hash table, this time using open addressing. Right now it only creates the table, adds to it / resolves collisions by walking the list, and searches the table for an element, walking until it finds it or None.
    Doesn't pass tests yet, initial commit.

---
+++ Algorithms-DataStructures/hash_open_addressing.py
@@ -0,0 +1,47 @@
+# Algorithms and Data Structures: Hash Tables & Hashing
+# 4.21.2016
+# @totallygloria
+
+"""
+Uses a python list to hold the values in the hash table.
+Hash function is a basic module arithmetic:
+   h(item) = item ^ 2 % table_size
+If there's a collision, resolves using open addressing.
+"""
+
+
+def create_table(des_len):
+    # Returns a list 2x the desired length, populated with None
+    return [None] * (2 * des_len)
+
+
+def insert_hash(item, table):
+    # Hashes item, if the slot is empty it inserts, otherwise it walks the table
+    # until it finds and open slot and puts it there.
+    size = len(table)
+    hashed_val = item^2 % size
+    if table[hashed_val] is None:
+        table[hashed_val] = item
+    else:
+        while table[hashed_val] is not None:
+            hashed_val += 1
+        table[hashed_val] = item
+
+def search_hash(item, table):
+    # Hashes item, checks if it's in the hash table - if it's not where it's expected
+    # to be, walks the table until a None is found. Returns a boolean.
+    hashed_val = item^2 % len(table)
+    if table[hashed_val] == item:
+        return True
+    elif table[hashed_val] == None:
+        return False
+    else:
+        if table[hashed_val] != item:
+            hashed_val += 1
+            while table[hashed_val] is not None:
+                if table[hashed_val] == item:
+                    return True
+        return False
+
+
+
---
+++ Algorithms-DataStructures/test_hash-open_addressing.py
@@ -0,0 +1,51 @@
+# Algorithms and Data Structures: hash Tables & Hashing
+# 4.19.2016
+# @totallygloria
+
+
+
+import unittest
+from hash_open_addressing import create_table, insert_hash, search_hash
+
+
+class HashClassTester(unittest.TestCase):
+
+    def test_create_table(self):
+        self.assertEqual(create_table(0), [])
+        self.assertEqual(create_table(1), [None, None])
+        self.assertEqual(create_table(2), [None, None, None, None])
+        self.assertEqual(create_table(8), ([None]*16))
+
+
+'''
+    def test_insert_hash(self):
+        h = create_table(5)
+        insert_hash(10, h)
+        self.assertEqual(h, [10, None, None, None, None, None, None, None, None, None])
+        insert_hash(11, h)
+        self.assertEqual(h, [10, 11, None, None, None, None, None, None, None, None])
+        insert_hash(12, h)
+        self.assertEqual(h, [10, 11, None, 12, None, None, None, None, None, None])
+        insert_hash(17, h)
+        self.assertEqual(h, [10, 11, 12, None, None, None, None, 17, None, None])
+        insert_hash(109, h)
+        self.assertEqual(h, [10, 11, 12, None, None, None, None, 17, None, 109])
+
+
+
+
+    def test_search_hash(self):
+        h = create_table(5)
+        extend_hash([10,11,12,106,108,109], h)
+        extend_hash([10,20,30,40], h)
+        self.assertTrue(search_hash(10, h))
+        self.assertTrue(search_hash(20, h))
+        self.assertTrue(search_hash(30, h))
+        self.assertTrue(search_hash(106, h))
+        self.assertTrue(search_hash(109, h))
+        self.assertFalse(search_hash(50, h))
+        self.assertFalse(search_hash(107, h))
+'''
+
+if __name__ == '__main__':
+    unittest.main()
