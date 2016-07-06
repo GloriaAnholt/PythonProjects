1da83ce43426fbcb8cab50b6546793669943408a
Gloria Guy <totallygloria@gmail.com>
2016-07-06 12:39:09 -0700

    Implemented recursive mergesort as a single function - splits until all lists are length 1, then compares left and right sides, adding the smaller to the list. Two additional loops at the end catch any remaining left or right elements. Passes basic check, no tests.

--- Algorithms-DataStructures/MergeSort.py
+++ Algorithms-DataStructures/MergeSort.py
@@ -13,13 +13,41 @@ on both halves. Once base case is reached on all branches, a merge is performed.
 
 def mergesort(unsorted):
 
-    if len(unsorted) > 1:
+    if len(unsorted) > 1:                   # If the list is longer than 1, keep splitting
         midpoint = len(unsorted) // 2
-        mergesort(unsorted[:midpoint])
-        mergesort(unsorted[midpoint:])
+        left = unsorted[:midpoint]
+        right = unsorted[midpoint:]
+        mergesort(left)
+        mergesort(right)
 
-    else:
-        print unsorted
+        l, r, i = 0, 0, 0
+
+        while l < len(left) and r < len(right):
+            if left[l] <= right[r]:         # If the left is smaller, add to list (preserves order)
+                unsorted[i] = left[l]
+                l += 1
+            else:
+                unsorted[i] = right[r]      # Else, add the right element
+                r += 1
+            i += 1
+
+        while l < len(left):                # If there's any left-list remaining, add them
+            unsorted[i] = left[l]
+            l += 1
+            i += 1
+
+        while r < len(right):               # Otherwise, if any r-side remaining, add them
+            unsorted[i] = right[r]
+            r += 1
+            i += 1
 
 
-mergesort([1,2,3,4,5,6,7,8,9,10])
+a = [98,76,54,43,21,0,-12]
+b = [1,2,3,4,5,8,1,2,3,9,-1,0]
+c = [9,8,7,6,5,4,3,2,1,0,-1]
+mergesort(a)
+mergesort(b)
+mergesort(c)
+print a
+print b
+print c
