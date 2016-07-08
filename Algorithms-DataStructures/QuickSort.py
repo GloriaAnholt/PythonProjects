203fd1861dd12e46b7c20472615baa8404c78bda
Gloria Guy <totallygloria@gmail.com>
2016-07-07 17:37:37 -0700

    QuickSort is now a class with a self.array attribute to keep track of the list being sorted. Instead of passing things about, it just modifies itself. The goal is to use this to better test the main exchanger method (since as a function it didn't have return values).

--- Algorithms-DataStructures/QuickSort.py
+++ Algorithms-DataStructures/QuickSort.py
@@ -10,52 +10,54 @@ to their correct location before or after the pivot point, finally swapping the 
 into place.
 """
 
+class QuickSort(object):
 
-def quicksort(unsorted):
+    def __init__(self):
+        self.array = []
 
-    if len(unsorted) <= 1:      # Nope out for an empty or one-element array
-        return unsorted
-    else:
-        exchanger(unsorted, 0, len(unsorted) - 1)
+    def quicksort(self):
+
+        if len(self.array) <= 1:      # Nope out for an empty or one-element array
+            return
+        else:
+            self.exchanger(0, len(self.array) - 1)
+
+    def exchanger(self, start, stop):
+
+        if stop - start > 0:  # Nope out for an empty set
+            pivot = self.getpivot(start, stop)
+            lmark = start
+            rmark = stop
+
+            while lmark <= rmark:
+                while self.array[lmark] < pivot:
+                    lmark += 1
+                while self.array[rmark] > pivot:
+                    rmark -= 1
+                if lmark <= rmark:
+                    self.array[lmark], self.array[rmark] = self.array[rmark], self.array[lmark]
+                    lmark += 1
+                    rmark -= 1
+
+            self.exchanger(start, rmark)  # sort the left side
+            self.exchanger(lmark, stop)  # sort the right side
+
+    def getpivot(self, start, stop):
+        # Select the pivot as the median of first-last-middle
+        first = self.array[start]
+        last = self.array[stop]
+        middle = self.array[(start + stop) // 2]
+
+        if (last <= first <= middle) or (middle <= first <= last):
+            return first                # Pivot is first element, or there are equals
+        elif (first < middle < last) or (last < middle < first):
+            return middle               # Pivot is midpoint
+        else:
+            return last                 # Pivot is last element
 
 
-def exchanger(unsorted, start, stop):
-
-    if stop - start > 0:  # Nope out for an empty set
-        pivot = getpivot(unsorted, start, stop)
-        lmark = start
-        rmark = stop
-
-        while lmark <= rmark:
-            while unsorted[lmark] < pivot:
-                lmark += 1
-            while unsorted[rmark] > pivot:
-                rmark -= 1
-            if lmark <= rmark:
-                unsorted[lmark], unsorted[rmark] = unsorted[rmark], unsorted[lmark]
-                lmark += 1
-                rmark -= 1
-
-        exchanger(unsorted, start, rmark)  # sort the left side
-        exchanger(unsorted, lmark, stop)  # sort the right side
-
-
-
-def getpivot(unsorted, start, stop):
-    # Select the pivot as the median of first-last-middle
-    first = unsorted[start]
-    last = unsorted[stop]
-    middle = unsorted[(start + stop) // 2]
-
-    if (last <= first <= middle) or (middle <= first <= last):
-        return first                # Pivot is first element, or there are equals
-    elif (first < middle < last) or (last < middle < first):
-        return middle               # Pivot is midpoint
-    else:
-        return last                 # Pivot is last element
-
-
-tester = [9,5,16,13,4,10,8,17,11,18,15,3,12,7,2,19,6,1,20,11,11,11,-10]
-print "starting list is:", tester
-quicksort(tester)
-print "final list is:", tester
+t1 = QuickSort()
+t1.array = [9, 5, 16, 13, 4, 10, 8, 17, 11, 18, 15, 3, 12, 7, 2, 19, 6, 1, 20, 11, 11, 11, -10]
+print "starting list is:", t1.array
+t1.quicksort()
+print "final list is:", t1.array
